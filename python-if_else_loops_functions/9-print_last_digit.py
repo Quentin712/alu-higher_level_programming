458a085fadabea8377bcbba4f5f5477f94ab03aa
Quentin712 <q.ntore@alustudent.com>
2026-06-22 12:17:00 +0200

    UPDATE 9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    """Print and return the last digit of a number"""
-    last_digit = number % 10
-    if last_digit < 0:
-        last_digit += 10
-    if number < 0 and last_digit > 0:
-        last_digit -= 10
+    """Print and return the last digit of a number, sign stripped"""
+    last_digit = abs(number) % 10
     print(last_digit, end="")
     return last_digit
