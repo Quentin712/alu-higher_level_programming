88da1566d0e8aa0ac58af1ff8d445b0b64d5357f
Quentin712 <q.ntore@alustudent.com>
2026-06-22 12:04:22 +0200

    Fizz Buzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    """Print numbers 1 to 100, replacing multiples of 3 with Fizz,
+    multiples of 5 with Buzz, multiples of both with FizzBuzz"""
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
