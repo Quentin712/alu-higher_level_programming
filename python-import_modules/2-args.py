f85a3bd6be4f570707da81a822d7efb031e4904f
Quentin712 <q.ntore@alustudent.com>
2026-06-24 12:22:23 +0200

    Count arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+def main():
+    n = len(argv) - 1
+
+    if n == 0:
+        print("{} argument.".format(n))
+    elif n == 1:
+        print("{} argument:".format(n))
+    else:
+        print("{} arguments:".format(n))
+
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    main()
