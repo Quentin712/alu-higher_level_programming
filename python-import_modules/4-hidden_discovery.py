de3125ea86b9af15fa9480bd2dafcf18ce7bc461
Quentin712 <q.ntore@alustudent.com>
2026-06-24 04:49:28 -0700

    Who are you? && Everything can be imported

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def main():
+    names = dir(hidden_4)
+    visible_names = []
+
+    for name in names:
+        if not name.startswith("__"):
+            visible_names.append(name)
+
+    visible_names.sort()
+
+    for name in visible_names:
+        print(name)
+
+
+if __name__ == "__main__":
+    main()
