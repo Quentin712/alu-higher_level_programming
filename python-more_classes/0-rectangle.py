3ec586d70de7a9db1e14407aa721c31b8b6c8aff
Quentin712 <q.ntore@alustudent.com>
2026-07-19 16:41:59 +0200

    Empty file 3

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 """This file has a class for a rectangle. It does nothing yet."""
+
+
 class Rectangle:
     """A rectangle. Right now it is empty and has no size or behavior."""
