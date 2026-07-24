1e63d08c0f8f5c24439e1cf08a7b4737029145d2
Quentin712 <q.ntore@alustudent.com>
2026-07-24 12:32:49 +0200

    Square #1 2

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,4 +8,5 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Initialize a new square with a validated size."""
+        self.integer_validator("size", size)
         super().__init__(size, size)
