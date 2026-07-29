2d13a8ebdea2b8b86d8251a6a59db297b459835d
Quentin712 <q.ntore@alustudent.com>
2026-07-29 16:52:25 +0200

    Add REVOKE statements
    Added multiple REVOKE statements for user_0d_1 to limit privileges.

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,3 +3,10 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
+REVOKE AUDIT_ABORT_EXEMPT ON *.* FROM 'user_0d_1'@'localhost';
+REVOKE AUTHENTICATION_POLICY_ADMIN ON *.* FROM 'user_0d_1'@'localhost';
+REVOKE FIREWALL_EXEMPT ON *.* FROM 'user_0d_1'@'localhost';
+REVOKE GROUP_REPLICATION_STREAM ON *.* FROM 'user_0d_1'@'localhost';
+REVOKE PASSWORDLESS_USER_ADMIN ON *.* FROM 'user_0d_1'@'localhost';
+REVOKE SENSITIVE_VARIABLES_OBSERVER ON *.* FROM 'user_0d_1'@'localhost';
+REVOKE TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_1'@'localhost';
