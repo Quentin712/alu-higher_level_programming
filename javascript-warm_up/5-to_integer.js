9fcda54ae84217a871bebdd65b95b857bd514328
Quentin712 <q.ntore@alustudent.com>
2026-09-22 14:26:33 +0200

    Fix errors

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const arg= process.arg[2];
-const num= parseINT(arg, 10);
+const arg = process.argv[2];
+const num = parseInt(arg, 10);
 
 if (isNaN(num)) {
   console.log('Not a number');
