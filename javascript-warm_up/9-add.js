1d80624042d0221ee5c463ead20c38cb3789e185
Quentin712 <q.ntore@alustudent.com>
2026-09-22 15:36:44 +0200

    Add

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const a = parseInt(process.argv[2], 10);
+const b = parseInt(process.argv[3], 10);
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(add(a, b));
