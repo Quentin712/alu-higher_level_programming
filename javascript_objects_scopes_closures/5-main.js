d84e98eb33e1a98afdbfb9dc81fb518e71eaaf11
Quentin712 <q.ntore@alustudent.com>
2026-09-26 11:56:58 +0200

    Refactor Square to extend Rectangle class

--- javascript_objects_scopes_closures/5-main.js
+++ javascript_objects_scopes_closures/5-main.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
-const Square = require('./5-square');
 
-const s1 = new Square(4);
-s1.print();
-s1.double();
-s1.print();
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (s) {
+    super(s, s);
+  }
+}
+
+module.exports = Square;
