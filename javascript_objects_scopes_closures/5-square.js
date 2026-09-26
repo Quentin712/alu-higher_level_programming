9f8dfb146fa9e8ac2ea81dd12ab5b5b2df78aca6
Quentin712 <q.ntore@alustudent.com>
2026-09-26 12:52:50 +0200

    Update 5-square.js

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,33 +1,10 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
 
-class Rectangle {
-  constructor (w, h) {
-    if (!Number.isInteger(w) || w <= 0 || !Number.isInteger(h) || h <= 0) {
-      return;
-    }
-    this.width = w;
-    this.height = h;
-  }
-
-  print () {
-    if (!this.width || !this.height) {
-      return;
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
   }
 }
 
-module.exports = Rectangle;
+module.exports = Square;
